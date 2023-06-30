05151767b6f521963f4585c2d0d376132ef7224c
Allen Helton <allenheltondev@gmail.com>
2023-06-30 16:37:29 -0500

    updates for load generator

--- api/load-generator/functions/send-request/index.js
+++ api/load-generator/functions/send-request/index.js
@@ -2,13 +2,22 @@ const axios = require('axios').default;
 
 exports.handler = async (state) => {
   let authToken;
-  
+
   const config = getAxiosConfig(state, authToken);
-  const response = await axios.request(config);
-  return response.data;
+  const iterations = state.iterations || 1;
+  const responses = [];
+  for (let i = 0; i < iterations; i++) {
+    const response = await axios.request(config);
+    responses.push(response.data);
+  }
+  if (iterations == 1) {
+    return responses[0];
+  }
+
+  return responses;
 };
 
-const getAxiosConfig = (state, authToken) => {
+const getAxiosConfig = (state) => {
   const config = {
     method: state.request.method,
     baseURL: state.request.baseUrl,
